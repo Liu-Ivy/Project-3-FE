c67522467844b8bef408cd6fa68954ab1a8c9a06
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-04-05 23:27:39 +0200

    wdpt starter point

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
-import auth from '../lib/auth-service';
+import React, { Component } from "react";
+import auth from "../lib/auth-service";
 
 class Login extends Component {
   state = {
     username: "",
-    password: "",
-  }
+    password: ""
+  };
 
-  handleFormSubmit = (event) => {
+  handleFormSubmit = event => {
     event.preventDefault();
-    const { username, password } = this.state
+    const { username, password } = this.state;
+  };
 
-    auth.login({ username, password })
-    .then( (user) => {
-      this.props.setUser(user)
-    })
-    .catch( error => console.log(error) )
-  }
-
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     const { username, password } = this.state;
     return (
       <form onSubmit={this.handleFormSubmit}>
         <label>Username:</label>
-        <input type="text" name="username" value={username} onChange={this.handleChange}/>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={this.handleChange}
+        />
         <label>Password:</label>
-        <input type="password" name="password" value={password} onChange={this.handleChange} />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={this.handleChange}
+        />
         <input type="submit" value="Login" />
       </form>
-    )
+    );
   }
 }
 
